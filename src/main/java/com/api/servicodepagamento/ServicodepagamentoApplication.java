5e9643e2f46a936d2268a8518ab240ddd2696f7a
Marco Tulio <marcotfrancosilva@gmail.com>
2023-03-11 17:58:42 -0300

    first commit

---
+++ src/main/java/com/api/servicodepagamento/ServicodepagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.api.servicodepagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicodepagamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicodepagamentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/servicodepagamento/ServicodepagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.servicodepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicodepagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
